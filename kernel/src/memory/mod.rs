b8ea4c058a36e2a124b093596c14c8e96e402419
thatmagicalcat <thatmagicalcat@hotmail.com>
2026-04-11 00:36:08 +0530

    feat(memory): global frame allocator

--- kernel/src/memory/mod.rs
+++ kernel/src/memory/mod.rs
@@ -1,12 +1,49 @@
+#![allow(static_mut_refs)]
+
 pub use bitmapframealloc::BitmapFrameAllocator;
-pub use tinyalloc::TinyAllocator;
+use spin::Mutex;
 
 mod bitmapframealloc;
-mod tinyalloc;
+mod vmm;
 pub mod heap;
 pub mod paging;
+mod tinyalloc;
+
+pub use vmm::{VMM, init_vmm};
 
 pub const PAGE_SIZE: usize = 1024 * 4;
+
+pub static mut GLOBAL_FRAME_ALLOCATOR: Option<Mutex<BitmapFrameAllocator>> = None;
+
+pub fn init_global_frame_allocator(memmap: &[*mut limine::limine_memmap_entry]) {
+    unsafe {
+        GLOBAL_FRAME_ALLOCATOR = Some(Mutex::new(BitmapFrameAllocator::new(memmap)));
+    }
+}
+
+pub fn lock_global_frame_allocator<'a>() -> spin::MutexGuard<'a, BitmapFrameAllocator> {
+    unsafe { GLOBAL_FRAME_ALLOCATOR.as_mut().unwrap().lock() }
+}
+
+pub fn allocate_frame() -> Option<Frame> {
+    unsafe {
+        GLOBAL_FRAME_ALLOCATOR
+            .as_mut()
+            .unwrap()
+            .lock()
+            .allocate_frame()
+    }
+}
+
+pub fn deallocate_frame(frame: Frame) {
+    unsafe {
+        GLOBAL_FRAME_ALLOCATOR
+            .as_mut()
+            .unwrap()
+            .lock()
+            .deallocate_frame(frame)
+    };
+}
 
 pub trait FrameAllocator {
     fn allocate_frame(&mut self) -> Option<Frame>;
@@ -26,6 +63,7 @@ impl Frame {
     pub const fn start_address(&self) -> usize {
         self.0 * PAGE_SIZE
     }
+
 
     pub const fn end_address(&self) -> usize {
         (self.0 + 1) * PAGE_SIZE
