3ec40cde84562e904733f3536d54d2ac22ffcc81
thatmagicalcat <thatmagicalcat@hotmail.com>
2026-04-21 08:49:41 +0530

    refactor: sys_mmap

--- kernel/src/syscall/mmap.rs
+++ kernel/src/syscall/mmap.rs
@@ -8,7 +8,7 @@ use crate::{
 
 bitflags::bitflags! {
     #[derive(Debug, Clone, Copy, PartialEq, Eq)]
-    pub struct MemMapFlags: i32 {
+    struct MemMapFlags: i32 {
         const MAP_FILE    = 0x00;
         const MAP_SHARED  = 0x01;
         const MAP_PRIVATE = 0x02;
@@ -17,7 +17,7 @@ bitflags::bitflags! {
     }
 
     #[derive(Debug, Clone, Copy, PartialEq, Eq)]
-    pub struct MmapProt: i32 {
+    struct MmapProt: i32 {
         const PROT_NONE  = 0x00;
         const PROT_READ  = 0x01;
         const PROT_WRITE = 0x02;
@@ -73,60 +73,82 @@ pub(crate) fn sys_mmap(
     }
 
     scheduler::with_current_task(|task| {
-        let addr = if flags.contains(MemMapFlags::MAP_FIXED) {
-            // alignment check
-            if !addr.is_multiple_of(memory::PAGE_SIZE) {
-                log::error!("MAP_FIXED address must be page-aligned");
-                return -errno::EINVAL as _;
-            }
-
-            // overlap check
-            if task.vmspace.find(addr).is_some() {
-                log::error!("MAP_FIXED address cannot be inserted into memory mapping");
-                return -errno::EINVAL as _;
-            }
-
-            addr
-        } else if addr == 0
-            || !addr.is_multiple_of(memory::PAGE_SIZE)
-            || task.vmspace.find(addr).is_some()
-        {
-            // if overlaps or not page-aligned, we need to find a new address
-            let layout = Layout::from_size_align(length, memory::PAGE_SIZE).unwrap();
-            let Some(new_addr) = task.vmspace.find_free_region(layout) else {
-                log::error!("OOM: no suitable free region found for {:?}", layout);
-                return -errno::ENOMEM as _;
-            };
-
-            new_addr
-        } else {
-            // we can just use it
-            addr
+        let addr = match get_mmap_addr(addr, length, flags, task) {
+            Ok(v) => v,
+            Err(v) => return v,
         };
 
         log::debug!(
             "sys_mmap(): addr={addr:#x}, length={length}, prot={prot:?}, flags={flags:?}, fd={fd}, offset={offset}",
         );
 
-        let mut flags = PageTableEntryFlags::empty();
-
-        if prot.contains(MmapProt::PROT_READ) {
-            flags |= PageTableEntryFlags::USER_ACCESSIBLE;
-        }
-
-        if prot.contains(MmapProt::PROT_WRITE) {
-            flags |= PageTableEntryFlags::USER_ACCESSIBLE | PageTableEntryFlags::WRITABLE;
-        }
-
-        if !prot.contains(MmapProt::PROT_EXEC) {
-            flags |= PageTableEntryFlags::NO_EXECUTE;
-        }
-
+        let flags = get_page_table_entry_flags(prot);
         let start = utils::align_down(addr as _, memory::PAGE_SIZE);
-        task.vmspace
+
+        if let Err(e) = task
+            .vmspace
             .insert(start, start + length, flags, MappingType::Anonymous)
-            .expect("Failed to insert memory mapping");
+        {
+            log::error!("Failed to insert VMA entry: {e:?}");
+            return -errno::EINVAL as _;
+        }
 
         start as _
+    })
+}
+
+fn get_page_table_entry_flags(prot: MmapProt) -> PageTableEntryFlags {
+    let mut flags = PageTableEntryFlags::empty();
+
+    if prot.contains(MmapProt::PROT_READ) {
+        flags |= PageTableEntryFlags::USER_ACCESSIBLE;
+    }
+
+    if prot.contains(MmapProt::PROT_WRITE) {
+        flags |= PageTableEntryFlags::USER_ACCESSIBLE | PageTableEntryFlags::WRITABLE;
+    }
+
+    if !prot.contains(MmapProt::PROT_EXEC) {
+        flags |= PageTableEntryFlags::NO_EXECUTE;
+    }
+
+    flags
+}
+
+fn get_mmap_addr(
+    hint: usize,
+    length: usize,
+    flags: MemMapFlags,
+    task: &scheduler::Task,
+) -> Result<usize, isize> {
+    Ok(if flags.contains(MemMapFlags::MAP_FIXED) {
+        // alignment check
+        if !hint.is_multiple_of(memory::PAGE_SIZE) {
+            log::error!("MAP_FIXED address must be page-aligned");
+            return Err(-errno::EINVAL as _);
+        }
+
+        // overlap check
+        if task.vmspace.find(hint).is_some() {
+            log::error!("MAP_FIXED address cannot be inserted into memory mapping");
+            return Err(-errno::EINVAL as _);
+        }
+
+        hint
+    } else if hint == 0
+        || !hint.is_multiple_of(memory::PAGE_SIZE)
+        || task.vmspace.find(hint).is_some()
+    {
+        // if overlaps or not page-aligned, we need to find a new address
+        let layout = Layout::from_size_align(length, memory::PAGE_SIZE).unwrap();
+        let Some(new_addr) = task.vmspace.find_free_region(layout) else {
+            log::error!("OOM: no suitable free region found for {:?}", layout);
+            return Err(-errno::ENOMEM as _);
+        };
+
+        new_addr
+    } else {
+        // we can just use it
+        hint
     })
 }
